60d053ecfadc2fc4adf60455949562cc2130ba6c
강현욱 <bug6868@naver.com>
2023-08-28 17:43:54 +0900

    60번 남의코드, 짧지만 느림

--- src/number60.java
+++ src/number60.java
@@ -42,4 +42,24 @@ public class number60 {
         }
         return answer;
     }
+
+
+    public int[] slowerShortSolution(String[] wallpaper) {
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+        for(int i=0; i< wallpaper.length;i++ ){
+            for(int j=0; j<wallpaper[i].length();j++){
+                if(wallpaper[i].charAt(j)=='#'){
+                    minX = Math.min(minX,i);
+                    minY = Math.min(minY,j);
+                    maxX = Math.max(maxX,i);
+                    maxY = Math.max(maxY,j);
+                }
+            }
+        }
+        return new int[]{minX,minY,maxX+1,maxY+1};
+    }
+
 }
