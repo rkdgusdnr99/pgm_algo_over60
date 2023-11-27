cafcdd8308db937b9fa1831eee83ca3fcb710259
강현욱 <bug6868@naver.com>
2023-11-28 00:14:13 +0900

    133번. 규칙을 찾는 문제였다. 처음부터 한칸한칸 최댓값을 찾는게 중요했고, 나는 마지막을 기준으로 봐서 다중 for문이 나와 시간 초과가 많이 발생했다. 결국 배열을 0으로 한번 감싸서 풀긴 했지만, 굳이 배열을 복사할 필요도 없었다.

---
+++ src/number133.java
@@ -0,0 +1,83 @@
+import java.util.Arrays;
+
+public class number133 {
+    public int BestSolution(int[][] triangle) {
+        for (int i = 1; i < triangle.length; i++) {
+            triangle[i][0] += triangle[i-1][0];
+            triangle[i][i] += triangle[i-1][i-1];
+            for (int j = 1; j < i; j++)
+                triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
+        }
+
+        return Arrays.stream(triangle[triangle.length-1]).max().getAsInt();
+    }
+    public int mySolution(int[][] triangle) {
+        int answer = 0;
+        int high = triangle.length;
+        int wide = triangle[high-1].length;
+        int[][] cotingTriangle = new int[high][wide+1];
+        for (int i = 0; i < high; i++) {
+            for (int j = 0; j <= i; j++) {
+                cotingTriangle[i][j + 1] = triangle[i][j];
+            }
+        }
+
+        for (int i = 1; i < high; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                cotingTriangle[i][j+1] += Math.max(cotingTriangle[i-1][j+1], cotingTriangle[i-1][j]);
+            }
+        }
+        for (int i = 0; i < wide; i++) {
+            answer = Math.max(answer, cotingTriangle[high-1][i+1]);
+        }
+
+        return answer;
+    }
+
+    public int timeOutSolution(int[][] triangle) {
+        int answer = 0;
+        int high = triangle.length - 1;
+        int wide = triangle[high].length - 1;
+
+        for (int i = 0; i <= wide; i++) {
+            answer = Math.max(answer, getSum(triangle, i));
+        }
+
+        return answer + triangle[0][0];
+    }
+
+    public static int getSum(int[][] triangle, int temp) {
+        int answer = 0;
+        int high = triangle.length - 1;
+        for (int i = high; i > 0; i--) {
+            if (temp == 0) {
+                answer += triangle[i][0];
+            }
+            else {
+                if (triangle[i][temp - 1] >= triangle[i][temp]) {
+                    answer += triangle[i][temp - 1];
+                    temp--;
+                }
+                else {
+                    answer += triangle[i][temp];
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // 테스트용 입력값 예시
+        int[][] triangle = {
+                {7},
+                {3, 8},
+                {8, 1, 0},
+                {2, 7, 4, 4},
+                {4, 5, 2, 6, 5}
+        };
+
+        number133 solution = new number133();
+//        int result = solution.solution(triangle);
+//        System.out.println("최대 합: " + result);
+    }
+}
